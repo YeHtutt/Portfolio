9cf4bc248cfbc73a12606a9d9e49fb223b2e5d7a
YeHtutAung <yehaung.nov@gmail.com>
2023-08-06 13:23:20 +0200

    responsive started

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,14 +8,36 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
   @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendbutton') sendbutton!: ElementRef;
+
+
+  requiredAlertName = false;
+  requiredAlertEmail = false;
+  requiredAlertMessage = false;
+
+  greenCheckMarkName = false;
+  greenCheckMarkEmail = false;
+  greenCheckMarkMessage = false;
+
+  emailSent = false;
+  showSpanMsg = false;
+  addClassToButton = false;
+
+  target!: HTMLInputElement;
   
 
   constructor(private http: HttpClient) {}
 
   async sendEmail() {
+    this.emailSent = true;
+    setTimeout(() => {
+      this.showSpanMsg = true;
+      this.addClassToButton = true;
+    }, 800);
     let nameInput = this.nameField.nativeElement; /*mit Ref #nameField Element(InputFeld) wird zugegriffen */
     let emailInput = this.emailField.nativeElement;
     let messageInput = this.messageField.nativeElement;
@@ -31,5 +53,64 @@ export class ContactComponent {
     });
   }
 
+  onBlur(event: Event) {
+    this.target = event.target as HTMLInputElement; 
+    this.checkInputState();
+  }
+
+  checkInputState() {
+    this.target.value.length > 0 ? this.changeInputFieldsGreen() : this.changeInputFieldsRed();
+  }
+
+  changeInputFieldsGreen() {
+    this.target.classList.add("content-filled");
+    this.target.classList.remove("empty-content");
+    this.target.classList.remove("bg-warning");
+  }
+
+  changeInputFieldsRed() {
+    this.target.classList.remove("content-filled");
+    this.target.classList.add("empty-content");
+    this.target.classList.add("bg-warning");
+  }
+
+  onInput(inputType: string) {
+    this.checkInputState();
+    this.checkInputValue(inputType);
+    this.greenCheckMarked(inputType);
+  }
+
+  checkInputValue(inputType: string) {
+    this.target.value.length > 0 ?  this.showRequiredMessage(inputType) : this.hideRequiredMessage(inputType);
+  }
+
+  showRequiredMessage(inputType: string) {
+    let htmlInputElement = 'requiredAlert' + inputType.charAt(0).toUpperCase() +inputType.slice(1);
+    this[`${htmlInputElement}`] = false;
+  }
+
+  hideRequiredMessage(inputType: string) {
+    let htmlInputElement = 'requiredAlert' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    this[`${htmlInputElement}`] = true;
+  }
+
+  greenCheckMarked(inputType: string) {
+    let htmlInputElement = 'greenCheckMark' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    this[`${htmlInputElement}`] = this.target.value.length > 0;
+  }
+
+  onFocus($event: Event, inputType: string) {
+    this.target = event.target as HTMLInputElement;
+
+    if(this.target.value.length === 0) {
+      this.target.classList.add("bg-warning");
+      this.showRequiredTextOnFocus(inputType);
+    }
+  }
+
+  showRequiredTextOnFocus(inputType: string) {
+    let htmlInputElement = 'requiredAlert' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    this[`${htmlInputElement}`] = true;
+  }
 
 }
